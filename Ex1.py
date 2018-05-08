c549b3e5ad264037259b277d5a6940547866a15a
J_ns <s8jemans@stud.uni-saarland.de>
2018-05-08 09:30:10 +0200

    changed variable name in ex1

--- Ex1.py
+++ Ex1.py
@@ -4,12 +4,12 @@ from backend import show_tensor
 S5A = np.load('S5A.npy')
 S5B = np.load('S5B.npy')
 S5C = S5A*S5B
-C_Trans = np.matrix.transpose(S5C)
-C_Inv = np.linalg.inv(S5C)
+S5C_Trans = np.matrix.transpose(S5C)
+S5C_Inv = np.linalg.inv(S5C)
 
 
 show_tensor({'A': S5A})
 show_tensor({'B': S5B})
 show_tensor({'C': S5C})
-show_tensor({'CTrans': C_Trans})
-show_tensor({'C_Inv': C_Inv})
+show_tensor({'CTrans': S5C_Trans})
+show_tensor({'C_Inv': S5C_Inv})
